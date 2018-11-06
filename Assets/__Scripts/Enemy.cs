75f92948cf72ed955121a3eedbe179cc0a103907
TCarter16 <trevorzero@gmail.com>
2018-11-06 16:48:51 -0500

    Initial commit

---
+++ Assets/__Scripts/Enemy.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+	public float speed = 10f;
+	public float fireRate = 0.3f;
+	public float health = 10;
+	public int score = 100;
+	public float showDamageDuration = 0.1f;
+	public float powerUpDropChance = 1f;
+	[Header("Set Dynamically: Enemy")]
+	public Color[] originalColors;
+	public Material[] materials;
+	public bool showingDamage = false;
+	public float damageDoneTime;
+	public bool notifiedOfDestuction = false;
+	protected BoundsCheck bndCheck;
+
+
+	public Vector3 pos
+	{
+		get{
+			return(this.transform.position);
+		}
+		set{
+			this.transform.position = value;
+		}
+	}
+
+	void Awake()
+	{
+		bndCheck = GetComponent<BoundsCheck>();
+
+		materials = Utils.GetAllMaterials(gameObject);
+		originalColors = new Color[materials.Length];
+
+		for (int i = 0; i<materials.Length; i++) {
+			originalColors[i] = materials[i].color;
+		}
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move();
+		if (showingDamage && Time.time > damageDoneTime)
+		{
+			UnShowDamage();
+		}
+		if(bndCheck != null && !bndCheck.isOnScreen)
+		{
+			if(pos.y < bndCheck.camHeight - bndCheck.radius)
+			{
+				Destroy(gameObject);
+			}
+		}
+	}
+	public virtual void Move()
+	{
+		Vector3 tempPos = pos;
+		tempPos.y -= speed * Time.deltaTime;
+		pos = tempPos;
+	}
+/*	void OnCollisionEnter(Collision coll) {
+		GameObject otherGO = coll.gameObject;
+
+		if (otherGO.tag == "ProjectileHero")
+		{
+			Destroy(otherGO);
+			Destroy(gameObject);
+		}
+		else{
+			print("Enemy hit by non-ProjectileHero: " + otherGO.name);
+		}
+	}*/
+	void OnCollisionEnter(Collision coll)
+	{
+		GameObject otherGO = coll.gameObject;
+		switch (otherGO.tag)
+		{
+			case "ProjectileHero":
+			Projectile p = otherGO.GetComponent<Projectile>();
+			ShowDamage();
+
+			if(!bndCheck.isOnScreen)
+			{
+				Destroy(otherGO);
+				break;
+			}
+			health -= Main.GetWeaponDefinition(p.type).damageOnHit;
+			if(health <= 0)
+			{
+				if (!notifiedOfDestuction) {
+					Main.S.ShipDestroyed(this);
+				}
+				notifiedOfDestuction = true;
+				Destroy(this.gameObject);
+			}
+			Destroy(otherGO);
+		//	Destroy(gameObject);
+		
+			break;
+
+			default:
+			print("Enemy hit by non-ProjectileHero: " + otherGO.name);
+			break;
+		}
+	}
+	void ShowDamage()
+	{
+		foreach(Material m in materials)
+		{
+			m.color = Color.red;
+		}
+		showingDamage = true;
+		damageDoneTime = Time.time + showDamageDuration;
+	}
+
+	void UnShowDamage()
+	{
+		for(int i=0; i<materials.Length; i++)
+		{
+			materials[i].color = originalColors[i];
+		}
+		showingDamage = false;
+	}
+}
---
+++ Assets/__Scripts/Hero.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour {
+	static public Hero S;
+	[Header("Set in Inspector")]
+//These fields control the movement of the ship
+	public float speed = 30;
+	public float rollMult = -45;
+	public float pitchMult = 30;
+	public float gameRestartDelay = 2f;
+	public GameObject projectilePrefab;
+	public float projectileSpeed = 40;
+	public Weapon[] weapons;
+
+	[Header("Set Dynamically")]
+	[SerializeField]
+	private float _shieldLevel = 1;
+	private GameObject lastTriggerGo = null;
+	public delegate void WeaponFireDelegate();
+	public WeaponFireDelegate fireDelegate;
+
+	void Start()
+	{
+		if (S == null)
+		{
+		S = this;
+		}
+		//fireDelegate += TempFire;
+		ClearWeapons();
+		weapons[0].SetType(WeaponType.blaster);
+	}
+
+	void Update()
+	{
+		float xAxis = Input.GetAxis("Horizontal");
+		float yAxis = Input.GetAxis("Vertical");
+		Vector3 pos = transform.position;
+		pos.x += xAxis * speed * Time.deltaTime;
+		pos.y += yAxis * speed * Time.deltaTime;
+		transform.position = pos;
+		transform.rotation = Quaternion.Euler(yAxis*pitchMult, xAxis*rollMult,0);
+
+	//	if (Input.GetKeyDown(KeyCode.Space))
+	//	{
+	//		TempFire();
+	//	}
+		if (Input.GetAxis("Jump") == 1 && fireDelegate != null)
+		{
+			fireDelegate();
+		}
+	}
+	void TempFire()
+	{
+		GameObject projGO = Instantiate<GameObject>(projectilePrefab);
+		projGO.transform.position = transform.position;
+		Rigidbody rigidB = projGO.GetComponent<Rigidbody>();
+		//rigidB.velocity = Vector3.up * projectileSpeed;
+		Projectile proj = projGO.GetComponent<Projectile>();
+		proj.type = WeaponType.blaster;
+		float tSpeed = Main.GetWeaponDefinition(proj.type).velocity;
+		rigidB.velocity = Vector3.up * tSpeed;
+	}
+			void OnTriggerEnter(Collider other){
+			Transform rootT = other.gameObject.transform.root;
+			GameObject go = rootT.gameObject;
+			//print("Triggered: " + go.name);
+			if (go == lastTriggerGo)
+			{
+				return;
+			}
+			lastTriggerGo = go;
+
+			if (go.tag == "Enemy")
+			{
+				shieldLevel--; 
+				Destroy(go);
+			}
+			else if (go.tag == "PowerUp") {
+				AbsorbPowerUp(go);
+			}
+			else {
+				print("Triggered: by non-Enemy" + go.name);
+			}
+		}
+		public void AbsorbPowerUp(GameObject go) {
+			PowerUp pu = go.GetComponent<PowerUp>();
+			switch(pu.type)
+			{
+				case WeaponType.shield: 
+				shieldLevel++;
+				break;
+
+				default: 				
+				if (pu.type == weapons[0].type) {
+					Weapon w = GetEmptyWeaponSlot();
+					if(w != null){
+						w.SetType(pu.type);
+					}
+				}
+					else{
+						ClearWeapons();
+						weapons[0].SetType(pu.type);
+					}
+					break;
+				}
+	          pu.AbsorbedBy(this.gameObject);
+			}
+
+		
+			public float shieldLevel {
+				get{
+					return(_shieldLevel);
+				}
+				set{
+					_shieldLevel = Mathf.Min(value, 4);
+					if (value < 0)
+					{
+						Destroy (this.gameObject);
+						Main.S.DelayedRestart(gameRestartDelay);
+					}
+				}
+			}
+			Weapon GetEmptyWeaponSlot(){
+				for( int i=0; i<weapons.Length; i++){
+					if(weapons[i].type == WeaponType.none){
+						return(weapons[i]);
+					}
+				}
+				return(null);
+			}
+			void ClearWeapons(){
+				foreach (Weapon w in weapons)
+				{
+					w.SetType(WeaponType.none);
+				}
+			}
+		}
